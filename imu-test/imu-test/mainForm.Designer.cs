5ec4909f6f021d1e8bdb58067574dba83c95cd5d
vic-w <vic.wudi@gmail.com>
2014-12-23 07:40:08 +0800

    Add main form

---
+++ imu-test/imu-test/mainForm.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace imu_test
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.glControl = new Tao.Platform.Windows.SimpleOpenGlControl();
+            this.SuspendLayout();
+            // 
+            // glControl
+            // 
+            this.glControl.AccumBits = ((byte)(0));
+            this.glControl.AutoCheckErrors = false;
+            this.glControl.AutoFinish = false;
+            this.glControl.AutoMakeCurrent = true;
+            this.glControl.AutoSwapBuffers = true;
+            this.glControl.BackColor = System.Drawing.Color.Black;
+            this.glControl.ColorBits = ((byte)(32));
+            this.glControl.DepthBits = ((byte)(16));
+            this.glControl.Location = new System.Drawing.Point(46, 30);
+            this.glControl.Name = "glControl";
+            this.glControl.Size = new System.Drawing.Size(258, 250);
+            this.glControl.StencilBits = ((byte)(0));
+            this.glControl.TabIndex = 0;
+            this.glControl.Paint += new System.Windows.Forms.PaintEventHandler(this.glControl_Paint);
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(523, 434);
+            this.Controls.Add(this.glControl);
+            this.Name = "mainForm";
+            this.Text = "IMU Test";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Tao.Platform.Windows.SimpleOpenGlControl glControl;
+    }
+}
+
---
+++ imu-test/imu-test/mainForm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using Tao.OpenGl;
+using Tao.Platform.Windows;
+
+namespace imu_test
+{
+    public partial class mainForm : Form
+    {
+        public mainForm()
+        {
+            InitializeComponent();
+            glControl.InitializeContexts();
+        }
+
+        private void glControl_Paint(object sender, PaintEventArgs e)
+        {
+           Gl.glClear(Gl.GL_COLOR_BUFFER_BIT);
+           Gl.glBegin(Gl.GL_TRIANGLES);
+           Gl.glColor3f(1.0f, 0.0f, 0.0f);
+           Gl.glVertex2d(0.0, 0.0);
+           Gl.glColor3f(0.0f, 1.0f, 0.0f);
+           Gl.glVertex2d(1.0, 0.0);
+           Gl.glColor3f(0.0f, 0.0f, 1.0f);
+           Gl.glVertex2d(0.5, 0.867);
+           Gl.glEnd();
+        }
+    }
+}
